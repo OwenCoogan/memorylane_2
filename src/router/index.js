54483e82156169fda6d8fb377ccce7d2494741ce
Owen Coogan <owen@seasonly.fr>
2021-10-13 19:03:43 +0200

    adding setup of vue router

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+const routes = [
+  {
+    path: '',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomePage.vue')
+  },
+  {
+    path: '/signin',
+    name: 'Sign-in',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signin.vue')
+  }
+] 
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    base: process.env.BASE_URL,
+    routes, // short for `routes: routes`
+  })
+
+
+export default router

8a125becf71838a1366b0cb955be3e07d0e155e9
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-01-05 17:32:22 +0100

    adding redirect to wanted location

---
+++ src/store/example.module.js
@@ -0,0 +1,9 @@
+export const posts = {
+  namespaced: true,
+  actions: {
+
+  },
+  mutations: {
+
+  }
+};
---
+++ src/store/geolocation.module.js
@@ -0,0 +1,49 @@
+export const geolocation = {
+  namespaced: true,
+  actions: {
+    currentLocation({ commit }) {
+      const { geolocation } = navigator;
+      commit('TOGGLE_LOADING', true);
+
+      return new Promise((resolve, reject) => {
+        if (geolocation) {
+          geolocation
+            .getCurrentPosition(({ coords }) => {
+              const currentPosition = {
+                long: coords.longitude,
+                lat: coords.latitude,
+              };
+              commit('CHANGE_GEOLOCATION', currentPosition);
+              commit('TOGGLE_LOADING', false);
+              resolve(currentPosition);
+
+            },
+            (err) => {
+              commit('TOGGLE_LOADING', false);
+              reject(err);
+            });
+        } else {
+          console.info('Geolocation is not supported by this browser.');
+          commit('TOGGLE_LOADING', false);
+          reject();
+        }
+      });
+    },
+    navigateToPostPoint({ commit },position){
+      const postPosition = {
+        long: position.long,
+        lat: position.lat,
+      }
+      commit('UPDATE_CURRENT_POSITION', postPosition);
+    }
+
+  },
+  mutations: {
+    CHANGE_GEOLOCATION(state, payload) {
+      state.geolocation = payload;
+    },
+    UPDATE_CURRENT_POSITION(state,payload) {
+      state.currentPosition = payload
+    },
+  }
+};

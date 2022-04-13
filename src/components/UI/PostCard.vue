1a4bd26ba866b22eca6b3d9b0932f81951c8baf1
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-04-13 22:16:00 +0200

    modified label

--- src/components/UI/PostCard.vue
+++ src/components/UI/PostCard.vue
@@ -19,7 +19,7 @@
       <div class="px-6 pt-4 pb-2">
       <span v-for="tag in tags" :key="tag.name" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{tag}}</span>
       </div>
-      <router-link  :to="'/Post/'+id"  class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Link mofo</router-link>
+      <router-link  :to="'/Post/'+id"  class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">View Post</router-link>
       <SwitchPosition :lat="lat" :long="long" />
     </div>
   </div>
